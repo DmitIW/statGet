bf3b6303ff8b7c6518aeb2047ab8c04b537d0bf2
d.ivakhnenko <d.ivakhnenko@ddos-guard.net>
2020-12-15 19:13:29 +0300

    ping tarantool connection after open

--- cmd/tConnector/connection.go
+++ cmd/tConnector/connection.go
@@ -18,5 +18,10 @@ func open(addr string, opts tarantool.Opts) *tarantool.Connection {
 	if result, err = tarantool.Connect(addr, opts); err != nil {
 		log.Fatalf("Conection opening error: %v\n", err)
 	}
+	if resp, err := result.Ping(); err != nil {
+		log.Fatalf("Tarantool connection is failed: %v\n", err)
+	} else {
+		log.Printf("Tarantool connection ping response: code = %v\n", resp.Code)
+	}
 	return result
 }
